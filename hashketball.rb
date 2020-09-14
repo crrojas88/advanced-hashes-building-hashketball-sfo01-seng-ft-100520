b97e5e208aaa590f9fd748ec25b1556a319afb0b
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-14 21:34:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       colors: ["Black", "White"],
       players: [
         {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
-        {player_name: "Reggie Evans", number: 30, shoe: 14, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "Reggie Evans", number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
         {player_name: "Brook Lopez", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
         {player_name: "Mason Plumlee", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
         {player_name: "Jason Terry", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0}
