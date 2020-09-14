b1cafa2232ce9d341aee0741f6b4d2c61152e9d2
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-14 21:34:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,9 +5,11 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        {player_name: "", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
-        
-        
+        {player_name: "Alan Anderson", number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
+        {player_name: "Reggie Evans", number: 30, shoe: 14, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "Brook Lopez", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "Mason Plumlee", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0},
+        {player_name: "Jason Terry", number: 0, shoe: 0, points: 0, rebounds: 0, assists: 0, steals: 0, blocks: 0, slam_dunks: 0}
         ]
     },
     away: {
