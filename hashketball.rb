60ef86af6582d13a4ba134b21c264ce4ddf2a519
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-14 21:29:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,8 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
       players: [
-        { :player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, }
+        {:player_name => "", :number => 0, :shoe => 0, :points => 0, :rebounds => 0, :assists => 0, :steals => 0, :blocks => 0, :slam_dunks => 0},
+        
         ]
     },
     away: {
